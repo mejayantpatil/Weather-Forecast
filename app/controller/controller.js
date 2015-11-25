c082e9e2022017d46ae1cb7cc20aa8fdfd6d5559
Jayant Patil <jayant.patil@yopmail.com>
2015-11-25 17:06:48 +0530

    first commit

---
+++ app/controller/controller.js
@@ -0,0 +1,9 @@
+/**
+ * Created by patil_j on 12/19/2014.
+ */
+var app=angular.module("testModule",[]);
+
+app.controller("TestController",function($scope){
+
+    $scope.Test="Testing ...";
+});
---
+++ app/ngApp.js
@@ -0,0 +1,4 @@
+/**
+ * Created by patil_j on 12/19/2014.
+ */
+var app=angular.module('myApp',['testModule']);
